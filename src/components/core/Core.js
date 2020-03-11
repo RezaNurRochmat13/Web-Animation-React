dfa96eab152cd440d817b173474aa1737e027350
rezanurrochmat@gmail.com <rezanurrochmat@gmail.com>
2020-03-11 12:25:11 +0700

    Add Card Components

--- src/components/core/Core.js
+++ src/components/core/Core.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Core.css';
+import Card from 'react-bootstrap/Card'
 
 class Core extends React.Component {
     render() {
@@ -7,19 +8,80 @@ class Core extends React.Component {
             <div className="Container">
                 <div className="row">
                     <div className="col-md-4">
-                        <div className="card">
-                            <div className="card-body">Content 1 appeared here</div>
-                        </div>
+                    <Card>
+                        <Card.Img variant="top" 
+                        src="https://previews.123rf.com/images/drekhann/drekhann1708/drekhann170800094/84589437-cute-cartoon-cats-and-dogs-with-different-emotions-sticker-collection-set-of-doodle-emoji-and-emotic.jpg"
+                        width="200px" />
+                        <Card.Body>
+                        <Card.Text>
+                            Some quick example text to build on the card title and make up the bulk
+                            of the card's content.
+                        </Card.Text>
+                        </Card.Body>
+                    </Card>
+                    <br/>
+                    <Card>
+                        <Card.Img variant="top" 
+                        src="https://previews.123rf.com/images/drekhann/drekhann1708/drekhann170800094/84589437-cute-cartoon-cats-and-dogs-with-different-emotions-sticker-collection-set-of-doodle-emoji-and-emotic.jpg"
+                        width="200px" />
+                        <Card.Body>
+                        <Card.Text>
+                            Some quick example text to build on the card title and make up the bulk
+                            of the card's content.
+                        </Card.Text>
+                        </Card.Body>
+                    </Card>
+                    <br />
                     </div>
                     <div className="col-md-4">
-                        <div className="card">
-                            <div className="card-body">Content 2 appeared here</div>
-                        </div>
+                    <Card>
+                        <Card.Img variant="top" 
+                        src="https://previews.123rf.com/images/drekhann/drekhann1708/drekhann170800094/84589437-cute-cartoon-cats-and-dogs-with-different-emotions-sticker-collection-set-of-doodle-emoji-and-emotic.jpg"
+                        width="200px" />
+                        <Card.Body>
+                        <Card.Text>
+                            Some quick example text to build on the card title and make up the bulk
+                            of the card's content.
+                        </Card.Text>
+                        </Card.Body>
+                    </Card>
+                    <br />
+                    <Card>
+                        <Card.Img variant="top" 
+                        src="https://previews.123rf.com/images/drekhann/drekhann1708/drekhann170800094/84589437-cute-cartoon-cats-and-dogs-with-different-emotions-sticker-collection-set-of-doodle-emoji-and-emotic.jpg"
+                        width="200px" />
+                        <Card.Body>
+                        <Card.Text>
+                            Some quick example text to build on the card title and make up the bulk
+                            of the card's content.
+                        </Card.Text>
+                        </Card.Body>
+                    </Card>
                     </div>
                     <div className="col-md-4">
-                        <div className="card">
-                            <div className="card-body">Content 3 appeared here</div>
-                        </div>
+                    <Card>
+                        <Card.Img variant="top" 
+                        src="https://previews.123rf.com/images/drekhann/drekhann1708/drekhann170800094/84589437-cute-cartoon-cats-and-dogs-with-different-emotions-sticker-collection-set-of-doodle-emoji-and-emotic.jpg"
+                        width="200px" />
+                        <Card.Body>
+                        <Card.Text>
+                            Some quick example text to build on the card title and make up the bulk
+                            of the card's content.
+                        </Card.Text>
+                        </Card.Body>
+                    </Card>
+                    <br />
+                    <Card>
+                        <Card.Img variant="top" 
+                        src="https://previews.123rf.com/images/drekhann/drekhann1708/drekhann170800094/84589437-cute-cartoon-cats-and-dogs-with-different-emotions-sticker-collection-set-of-doodle-emoji-and-emotic.jpg"
+                        width="200px" />
+                        <Card.Body>
+                        <Card.Text>
+                            Some quick example text to build on the card title and make up the bulk
+                            of the card's content.
+                        </Card.Text>
+                        </Card.Body>
+                    </Card>
                     </div>
                 </div>
             </div>
